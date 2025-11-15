199d0ec276eac06b57adc96ce4b5f95b583597a3
connorvogelaar95 <connorvogelaar95@gmail.com>
2025-11-15 18:07:14 +0000

    Update menuoptions.js

--- js/menuoptions.js
+++ js/menuoptions.js
@@ -1,1 +1,7 @@
-
+/* menu items expand*/
+document.querySelectorAll(".item-heading").forEach((heading) => {
+    heading.addEventListener("click", () => {
+        const section = heading.parentElement;
+        section.classList.toggle("open");
+    });
+});
