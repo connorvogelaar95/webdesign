ff2da4d7f0ba47b03fff7ca98862a21fb61d96a3
connorvogelaar95 <connorvogelaar95@gmail.com>
2025-11-15 18:14:38 +0000

    Edit comment to specify author for menu items expand

--- js/menuoptions.js
+++ js/menuoptions.js
@@ -1,4 +1,4 @@
-/* menu items expand*/
+/* edited by Connor, menu items expand*/
 document.querySelectorAll(".item-heading").forEach((heading) => {
     heading.addEventListener("click", () => {
         const section = heading.parentElement;
